3a3cc1f3bc06145466143cf887eeaae145bb3e02
Steve Yardumian <syardumi@gmail.com>
2017-06-26 17:00:23 -0400

    Special style shouldn’t go into the future (past today) [take 2]

--- CalendarPicker/Day.js
+++ CalendarPicker/Day.js
@@ -71,7 +71,7 @@ export default function Day(props) {
     }
     
     //set special day style (when start day is specified)
-    if (specialStyleStartDate && thisDay >= specialStyleStartDate && specialStyleStartDate <= today){
+    if (specialStyleStartDate && thisDay >= specialStyleStartDate && thisDay <= today){
     	specialDayStyle = specialStyle;
     }
 
